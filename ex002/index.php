74daf594dc86093e47f7af59ec6ff98a50b8592e
RafaelLanaDS <rafael.lanads@outlook.com>
2025-08-09 00:26:57 -0300

    09/08/25

--- ex002/index.php
+++ ex002/index.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste</title>
+    <title>PHP</title>
 </head>
 <body>
-    <h1>Testando php no github</h1>
+    <h1>Exemplo de PHP</h1>
     <?php 
-        echo "Deu Certo";
+        date_default_timezone_set("America/sao_Paulo"); //GMT-3
+        echo "Hoje é dia " . date("d/M/Y");
+        echo " e a hora atual é " . date("G:i:s ")
     ?>
 </body>
 </html>
