2e4df95a14fb59168d66c9033b4ce8699affe054
RafaelLanaDS <rafael.lanads@outlook.com>
2025-09-14 19:05:31 -0300

    commit

--- lista_exercicios01.php/ex006.php/index.php
+++ lista_exercicios01.php/ex006.php/index.php
@@ -9,28 +9,38 @@
 <body>
     <?php 
         //capiturando os dados do formulario retroalimentado
-        $div = $_GET['dividendo'] ??0;
-        $divisor = $_GET['divisor'] ??0;
+        $div = $_GET['dividendo'] ?? 0;
+        $divisor = $_GET['divisor'] ?? 1;
 
     ?>
     <main>
         <h1>Anatomia de uma divisão</h1>
         <form action="<?php echo $_SERVER['PHP_SELF']?>" method="get">
             <label for="dividendo">dividendo</label>
-            <input type="number" name="dividendo">
+            <input type="number" name="dividendo" value="<?=$div?>" >
             <label for="divisor">Divisor</label>
-            <input type="number" name="divisor">
+            <input type="number" name="divisor" value="<?=$divisor?>">
             <input type="submit" value="Analisar">
         </form>
     </main>
     <section id="resultado">
-        <h1>Estrutura da divisão</h1>
-        <tbody>
+        <h2>Estrutura da Divisão</h2>
+        <?php 
+            //calculos
+            $quociente = intdiv($div, $divisor); // intdiv pega o valor inteiro do calculo
+            $resto = $div % $divisor;
+        ?>
+        <table class="divisao">
             <tr>
-                <td>sao paulo</td>
-                <td>554515425</td>
+                <td><?=$div?></td>
+                <td><?=$divisor?></td>
             </tr>
-        </tbody>
+            <tr>
+                <td><?=$resto?></td>
+                <td><?=$quociente?></td>
+            </tr>
+        </table>
     </section>
+    </main>
 </body>
 </html>
