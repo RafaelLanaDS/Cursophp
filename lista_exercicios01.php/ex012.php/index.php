d5faef8034f500d174d1e4dbb0fa25908846378e
RafaelLanaDS <rafael.lanads@outlook.com>
2025-10-08 20:37:09 -0300

    commit

--- lista_exercicios01.php/ex012.php/index.php
+++ lista_exercicios01.php/ex012.php/index.php
@@ -7,19 +7,42 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <?php 
+        $total = $_GET['seg']??0;
+    ?>
     <main>
         <h1>Calculadora de Tempo</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="">Qual é o total de segundos</label>
-            <input type="number" name="" id="">
+            <input type="number" name="seg" id="seg" min="0" step="1" require>
             <input type="submit" value="Calcular">
         </form>
     </main>
     <section>
         <h2>Totalizando Tudo</h2>
-        <?php 
-            echo "Analisando o valor que voce digitou <strong>{} segundos</strong> equivalem a um total de:";
-            echo "<ul><li>Semanas</li><li>Dias</li><li>Horas</li><li>Minutos</li><li>Segundos</li></ul>"
+        <?php
+            
+            $sobra = $total;
+            //semanas
+            $semanas = (int)($sobra/604800);
+            $sobra = $sobra % 604800;
+
+            //total dias 
+            $dia = (int)($sobra/86040);
+            $sobra = $sobra % 86400;
+
+            //total horas 
+            $horas = (int)($sobra/3600);
+            $sobra = $sobra % 3600;
+
+            //minutos
+            $minutos = (int)($sobra/60);
+            $sobra = $sobra % 60;
+
+            $segundo = $sobra;
+
+            echo "Analisando o valor que voce digitou " . number_format($total, 2,',','.') . " equivalem a um total de:";
+            echo "<ul><li>$semanas Semanas</li><li>$dia Dias</li><li>$horas Horas</li><li>$minutos Minutos</li><li>$segundo Segundos</li></ul>";
         ?>
     </section>
 </body>
